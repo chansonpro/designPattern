286c8219e45d8cb6ab0db5faedefc802354a2809
Chanson <chansonpro@outlook.com>
2017-11-16 11:28:38 +0800

    观察者模式

---
+++ src/main/java/com/baidu/service/Observer.java
@@ -0,0 +1,10 @@
+package com.baidu.service;
+
+/**
+ * User: chanson-pro
+ * Date-Time: 2017-11-16 11:26
+ * Description:观察者接口
+ */
+public interface Observer {
+    void update();
+}
---
+++ src/main/java/com/baidu/service/Subject.java
@@ -0,0 +1,17 @@
+package com.baidu.service;
+
+import java.util.Observer;
+
+/**
+ * User: chanson-pro
+ * Date-Time: 2017-11-16 11:15
+ * Description:主题（发布者、被观察者）
+ */
+public interface Subject {
+    //注册观察者
+    void registerObserver(Observer observer);
+    //移除观察者
+    void removeObserver(Observer observer);
+    //通知观察者
+    void notifyObserver();
+}
